8b97d1e9c9a6c0e5dc5a8bb31e350010bf86c502
ntzwilly <ntzwilly@gmail.com>
2022-07-28 16:39:19 +0200

    Build a the header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,47 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import {
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+  UserIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerShown: false
+      headerShown: false,
     });
   }, []);
 
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">HomeScreen</Text>
+    <SafeAreaView className="mt-10 bg-white, pt-5">
+      <Text className="text-red-500">
+
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View className='flex-1'>
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+
+          <UserIcon size={35} color="#00CCBB" />
+        </View>
+      </Text>
     </SafeAreaView>
   );
 };
