1c5fdb026e732728e4c7ebc9b4b2bf8541a38e1e
ntzwilly <ntzwilly@gmail.com>
2022-08-08 18:45:16 +0200

    Configure Sanity for Restaurant

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -55,18 +55,6 @@ const FeaturedRow = ({ id, title, description }) => {
           long={20}
           lat={0}
         />
-        <RestaurantCard
-          id={123}
-          imgUrl="https://links.papareact.com/gn7"
-          title="Yo! Sushi"
-          rating={4.5}
-          genre="Japanese"
-          address="123 Main St"
-          short_description="This is a test description"
-          dishes={[]}
-          long={20}
-          lat={0}
-        />
       </ScrollView>
     </View>
   );
