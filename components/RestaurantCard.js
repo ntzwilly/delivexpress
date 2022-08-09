5e50921fb823f73a8d2b2c53db7065f47caf52e1
ntzwilly <ntzwilly@gmail.com>
2022-08-09 14:25:21 +0200

    Style the image

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -21,7 +21,7 @@ const RestaurantCard = ({
         source={{
           uri: urlFor(imgUrl).url(),
         }}
-        className='h-36 w-64 rounded-sm'
+        className='h-36 w-full rounded-sm'
       />
       <View className='px-3 pb-4'>
         <Text className='font-bold text-lg pt-2'>{title}</Text>
