8c531d0274436a3a2f2eaf2d4e0ab65e913fea18
ntzwilly <ntzwilly@gmail.com>
2022-08-24 21:13:18 +0200

    Create basketscreen

--- components/BasketScreen.js
+++ components/BasketScreen.js
@@ -1,7 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const BasketScreen = () => {
+  const navigation = useNavigation()
   return (
     <View>
       <Text>BasketScreen</Text>
