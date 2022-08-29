d53559ef86ddd259e7dc7a7e5185e6a09a4084d1
ntzwilly <ntzwilly@gmail.com>
2022-08-29 13:28:48 +0200

    Install MapView

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,24 +1,32 @@
 import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import React, {useEffect} from "react";
 import * as Animatable from "react-native-animatable";
+import * as Progress from "react-native-progress";
+import { useNavigation } from "@react-navigation/native";
 
 const PreparingOrderScreen = () => {
+  const navigation = useNavigation()
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("Delivery")
+    }, 4000)
+  }, [])
   return (
     <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
-      < Animatable.Image 
+      <Animatable.Image
         source={require("../assets/orderLoading.gif")}
         animation="slideInUp"
         iterationCount={1}
-        className="h-96 w-96"
+        className="h-64 w-64 rounded-full"
       />
-      <Animatable.Text 
+      <Animatable.Text
         animation="slideInUp"
         iterationCount={1}
-        className="text-lg my-10 text-white font-bold text-center"
+        className="text-lg my-10 text-white font-bold text-center px-5"
       >
         Waiting for Restaurant to accept your order!
       </Animatable.Text>
-
+      <Progress.Circle size={60} indeterminate={true} color="white" />
     </SafeAreaView>
   );
 };
